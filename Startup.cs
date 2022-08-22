3e41e3a4c0e4c508c29b28d90d5fe6e8b38283e8
sector viper <sectorviper1@gmail.com>
2022-08-23 02:10:39 +0300

    finshed

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,17 @@
-using CmsShoppingCart.InfraStructure;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CmsShoppingCart.InfraStructure;
+using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace CmsShoppingCart
 {
@@ -25,10 +27,29 @@ namespace CmsShoppingCart
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMemoryCache();
+            services.AddSession(options =>
+            {
+                //options.IdleTimeout = TimeSpan.FromSeconds(2)
+                //options.IdleTimeout = TimeSpan.FromDays(2)
+            });
+
+            services.AddRouting(options => options.LowercaseUrls = true);
+
             services.AddControllersWithViews();
 
-            services.AddDbContext<CmsShoppingCartContext>(options => options.UseSqlServer
-            (Configuration.GetConnectionString("CmsShoppingCartContext")));
+            services.AddDbContext<CmsShoppingCartContext>(options => options.UseSqlServer(Configuration.GetConnectionString("CmsShoppingCartContext")));
+
+            services.AddIdentity<AppUser, IdentityRole>(options =>
+            {
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireDigit = false;
+            })
+                    .AddEntityFrameworkStores<CmsShoppingCartContext>()
+                    .AddDefaultTokenProviders();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -49,26 +70,29 @@ namespace CmsShoppingCart
 
             app.UseRouting();
 
+            app.UseSession();
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
-                    "pages" , 
+                    "pages",
                     "{slug?}",
-                    defaults: new { controller = "Pages", action = "Page"}
-                    );
+                    defaults: new { controller = "Pages", action = "Page" }
+                );
 
                 endpoints.MapControllerRoute(
                     "products",
                     "products/{categorySlug}",
                     defaults: new { controller = "Products", action = "ProductsByCategory" }
-                    );
+                );
 
                 endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-                  );
+                );
 
                 endpoints.MapControllerRoute(
                     name: "default",
